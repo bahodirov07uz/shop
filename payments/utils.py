8991453299dc0b810a71fd695cd5110a0644c8b7
bahodirov07uz <muhammadyahyobahodirov376@gmail.com>
2025-07-30 18:13:52 +0500

    i hope last commit:)

--- payments/utils.py
+++ payments/utils.py
@@ -2,4 +2,8 @@ import hashlib
 from django.conf import settings
 
 def verify_signature(data):
-    return True  # vaqtinchalik
+    secret = settings.CRYPTOCLOUD_WEBHOOK_SECRET
+    raw = data.get("amount", "") + data.get("currency", "") + data.get("status", "") + \
+          data.get("order_id", "") + data.get("uuid", "") + secret
+    signature = hashlib.sha256(raw.encode()).hexdigest()
+    return signature == data.get("sign")
